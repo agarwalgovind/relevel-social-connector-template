d37edcee2392d3727fb051c4c8ea14ea64794ec3
govindagarwal <Govi@fico12345>
2021-06-29 01:10:18 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.css";
+
+const App = () => {
+
+  return (
+      <div>
+          {/**YOUR CODE START HERE**/}
+      </div>
+  );
+};
+
+export default App;
